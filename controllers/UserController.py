308915cc527dbe10ec348e88423278df53d08111
Daniel <daniel1maymon@gmail.com>
2023-07-04 18:55:14 +0300

    Add user blueprint for handling user-related routes
    - Create user blueprint in 'Usercontroller.py'
    - Define '/test' endpoint for testing purposes
    - Register user blueprint in 'server_app.py' with '/users' prefix
    
    This commit separetes user-related routes into a blueprint for better organization and maintainabilty.
    The new '/users/test' endpoint allows testing the functionality of the user module independently.

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,7 +1,19 @@
-from server_app import app
-from flask import jsonify
+import sys
+import os
+from flask import jsonify, Blueprint
 
-@app.route("/test", methods='GET')
+user_blueprint = Blueprint("user_blueprint", __name__)
+
+# Get the current directory and the parent directory
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+
+sys.path.append(parent_dir)
+
+# from server_app import app
+
+
+@user_blueprint.route("/test", methods=["GET"])
 def test():
     print("It's work!")
     
--- server_app.py
+++ server_app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from controllers.UserController import user_blueprint
 
 app = Flask(__name__)
+
+# Blueprints
+app.register_blueprint(user_blueprint, url_prefix='/users')
+
+
+
 print()
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080)
+    app.run(host="127.0.0.1", port=8080, debug=True)
